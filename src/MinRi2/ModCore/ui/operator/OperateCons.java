23e0036cdb762b27da454ca71513e02d402d4aa6
minri2 <2275045670@qq.com>
2024-02-10 20:48:45 +0800

    Update

---
+++ src/MinRi2/ModCore/ui/operator/OperateCons.java
@@ -0,0 +1,25 @@
+package MinRi2.ModCore.ui.operator;
+
+import arc.scene.*;
+
+public interface OperateCons{
+    default boolean keepWithinStage(){
+        return true;
+    }
+
+    default boolean alizable(Element element){
+        return true;
+    }
+
+    default void onDragged(float deltaX, float deltaY){
+    }
+
+    default void onResized(float deltaX, float deltaY){
+    }
+
+    default void onAligned(Element snap, int alignFrom, int alignTo){
+    }
+
+    default void onReleased(){
+    }
+}
