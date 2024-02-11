50095975d6f870aadbcf66206504c718e20c7cd6
minri2 <2275045670@qq.com>
2024-02-11 13:57:46 +0800

    Have a test.

--- importer/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-package main.java;
-
-/**
- * @author minri2
- * Create by 2024/2/11
- */
-public class Main{
-    public static String TEST = "TESTIMPORTER";
-}
---
+++ importer/src/main/java/MinRi2/importer/MinModCoreImporter.java
@@ -0,0 +1,13 @@
+package main.java.MinRi2.importer;
+
+import arc.util.*;
+
+/**
+ * @author minri2
+ * Create by 2024/2/11
+ */
+public class MinModCoreImporter{
+    public static void importMinModCore(String version){
+        Log.info("Mod require import: @", version);
+    }
+}
